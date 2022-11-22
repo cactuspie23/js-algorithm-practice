cbc3cf68f4ce69bef80baa365b50597ad1f756ef
AlyssaD <pink_lime@live.com>
2022-11-22 14:20:42 -0500

    first attempt

--- frequency-counter-pattern.js
+++ frequency-counter-pattern.js
@@ -1,9 +1,11 @@
+// Write a function called same, which accepts two arrays. The function should return true if every value in the array has it's corresponding value squared n the second array. The frequency of values must be the same. 
+
 function same(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false
   }
   for (let i = 0; i < arr1.length; i++) {
-    let correctIndex = arr2.indexOf(arr[i] ** 2)
+    let correctIndex = arr2.indexOf(arr1[i] ** 2)
     if (correctIndex === -1) {
       return false
     }
@@ -11,3 +13,5 @@ function same(arr1, arr2) {
   }
   return true
 }
+
+console.log(same([1,2,3,2], [9,4,4,1]))
