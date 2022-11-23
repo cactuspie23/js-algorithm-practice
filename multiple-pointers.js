c067354f10c01f572fa882851a25c3d3e6d56b69
AlyssaD <pink_lime@live.com>
2022-11-23 09:19:40 -0500

    second attempt

--- multiple-pointers.js
+++ multiple-pointers.js
@@ -10,4 +10,26 @@ function sumZero(arr) {
   }
 }
 
+// Time complexity O(n^2) due to nested loops
+
 console.log(sumZero([-4,-3,-2,-1,0,1,2,5]))
+
+function sumZero(arr) {
+  let left = 0
+  let right = arr.length -1
+  while (left < right) {
+    let sum = arr[left] + arr[right]
+    if (sum === 0) {
+      return [arr[left], arr[right]]
+    } else if (sum > 0) {
+      right --
+    } else {
+      left ++
+    }
+  }
+}
+
+// Time complexity O(n)
+// Space complexity O(1)
+
+console.log(sumZero([-4,-3,-2,-1,0,1,2,3,10]))
