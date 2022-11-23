a324894056a2a41ca80a4f2e7114ee3b2f528926
AlyssaD <pink_lime@live.com>
2022-11-23 10:13:13 -0500

    update solution

--- unique-values.js
+++ unique-values.js
@@ -1,6 +1,7 @@
 // Implement a function called countUniqueValues, which accepts a sorted array, and counts the unique values in the array. There can be negative numbers in the array, but it will always be sorted. 
 
 function countUniqueValues(arr) {
+  if (arr.length === 0) return 0
   let i = 0
   for (let j = 1; j < arr.length; j++) {
     if (arr[i] !== arr[j]) {
@@ -10,5 +11,7 @@ function countUniqueValues(arr) {
   }
   return i + 1
 }
+
+// O(n) complexity because it only loops once
 
 console.log(countUniqueValues([1,1,1,2,3,3,4,4,5,6]))
