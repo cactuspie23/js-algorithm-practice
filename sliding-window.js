8099f16dcbb60ce63e8dad1df75a4eae5ae41688
AlyssaD <pink_lime@live.com>
2022-11-25 09:40:02 -0500

    second attempt

--- sliding-window.js
+++ sliding-window.js
@@ -16,3 +16,23 @@ function maxSubarraySum(arr, num) {
   }
   return max
 }
+
+// complexity is O(n^2) for the nested loop
+
+function maxSubarraySum(arr, num) {
+  let maxSum = 0
+  let tempSum = 0
+  if (arr.length < num) return null
+  for (let i = 0; i < num; i++) {
+    maxSum += arr[i]
+  }
+  tempSum = maxSum
+  for (let i = num; i < arr.length; i++) {
+    tempSum = tempSum - arr[i - num] + arr[i]
+    maxSum = Math.max(maxSum, tempSum)
+  }
+  return maxSum
+}
+
+// complexity O(n) two separate loops
+// only goes through the array and adds numbers once
