888c9f063583a72f7b51547280bb42cd26b48ffe
AlyssaD <pink_lime@live.com>
2022-12-01 12:09:46 -0500

    recursive function

--- recursion.js
+++ recursion.js
@@ -1,2 +1,16 @@
 // the call stack data structure
 // when JS sees the return keyword or when function ends the compiler will remove(pop)
+// recursive functions keep pushing new functions onto call stack
+// the base case is when the function ends
+
+function countDown(num) {
+  if(num <= 0) {
+    console.log('All Done!')
+    return
+  }
+  console.log(num)
+  num--
+  countDown(num)
+}
+
+countDown(5)
