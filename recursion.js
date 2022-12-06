fc99f4b6c6aca215e85d6eb37149bff4ae52aac9
AlyssaD <pink_lime@live.com>
2022-12-06 09:38:08 -0500

    add helper functions

--- recursion.js
+++ recursion.js
@@ -21,3 +21,21 @@ function factorial(num) {
 }
 
 console.log(factorial(5))
+
+// helper method recursion
+// outter function has inner helper function that calls itself
+
+function collectOdds(nums){
+  let result = []
+  function helper(helperInput){
+    if(helperInput.length === 0) return
+    if(helperInput[0] % 2 !== 0){
+      result.push(helperInput[0])
+    }
+    helper(helperInput.slice(1))
+  }
+  helper(nums)
+  return result
+}
+
+console.log(collectOdds([1,2,3,4,5,6,7,8,9]))
