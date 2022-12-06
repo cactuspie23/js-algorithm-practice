30f7291d0419a53f5d9ec3d64b623644bef89fa0
AlyssaD <pink_lime@live.com>
2022-12-06 10:01:06 -0500

    power solution

--- recursion.js
+++ recursion.js
@@ -60,3 +60,11 @@ console.log(collectOddValues([1,2,3,4,5]))
 
 // Write a function called power which accepts a base and an exponent. The function should return the power of the base to the exponent. 
 
+function power(base, exp){
+  if(exp === 0) return 1
+  return base * power(base, exp -1)
+}
+
+power(2,0) // 1
+power(2,2) // 4
+power(2,4) // 16
