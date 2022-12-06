1000fb54ed083b4b6c4bb49ed96b36949d24bb19
AlyssaD <pink_lime@live.com>
2022-12-06 10:21:41 -0500

    solve product of array

--- recursion.js
+++ recursion.js
@@ -82,3 +82,8 @@ factorial(4) // 24
 console.log(factorial(7)) // 5040
 
 // Write a function called productOfArray which takes in an array of numbers and returns the product of them all.
+
+function productOfArray(arr){
+  if(arr.length === 0) return 1
+  return arr[0] * productOfArray(arr.slice(1))
+}
